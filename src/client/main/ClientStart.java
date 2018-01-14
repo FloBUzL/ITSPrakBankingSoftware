6c8a7d5b8238ef5e888a7060133737da71038710
Florian Berberich <florian.berberich@student.uni-luebeck.de>
2018-01-14 19:10:00 +0100

    documented client.main

--- src/client/main/ClientStart.java
+++ src/client/main/ClientStart.java
@@ -3,7 +3,16 @@ package client.main;
 import client.connection.Client;
 import shared.exception.NotEnoughArgumentsExceptions;
 
+/**
+ * the entry point for the client
+ * @author Florian
+ */
 public class ClientStart {
+    /**
+     * starts the application
+     * @param args 0: the host to connect to, 1: the socket to connect to
+     * @throws NotEnoughArgumentsExceptions
+     */
     public static void main(String[] args) throws NotEnoughArgumentsExceptions {
 	if(args.length < 2) {
 	    throw new NotEnoughArgumentsExceptions("specify database file and server port");
